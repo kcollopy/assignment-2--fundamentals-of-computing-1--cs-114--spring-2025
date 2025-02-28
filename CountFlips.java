b3dde0f460f98d1ec32b05a2e532ea5a4636e5d3
kcollopy <kelly.collopy21@gmail.com>
2025-02-27 20:12:53 -0500

    Coin flips

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,23 @@
 public class CountFlips {
   public static void main(String[] args) {
+    Coin myCoin;
+    int count = 0;
+    int headCount = 0;
+    
+    myCoin = new Coin();
+
+      while (count<=100) {
+        myCoin.flip();
+
+          if (myCoin.isHeads()){
+
+           headCount++; 
+          } 
+
+        count++;
+      }
+      
+        System.out.print("The coin landed on heads " + headCount  + " times and landed on tails " + (100 - headCount) + " out of 100 coin flips");
+
   }
 }
